f1ea68a1b2074cb7ef4098b622ba58f85c57c317
snaider10 <snaiderortega10@gmail.com>
2025-03-01 03:07:04 -0500

    Add basic Items

--- furtunecookie/src/App.jsx
+++ furtunecookie/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import './App.css'
+import './style.css'
 
 function App() {
   //define state variables
@@ -26,14 +26,18 @@ function App() {
     }
   },[active])
   return (
-    <main>
-      <section>
-        <div>
+    <main className='container h-screen flex justify-center items-center'>
+      <section className='container flex gap-4 p-6 rounded-2xl justify-center items-center shadow-2xl'>
+        <div className='w-96 h-96 bg-yellow-200 rounded-full'>
+          <img src='cookie.jpg' alt='cookie' className='rounded-full h-full w-full' />
         </div>
-        <article>
-          <h1>This is your fortune!</h1>
+        <article className='flex flex-col gap-2'>
+          <h1 className='font-bold text-2xl'>Prueba tu fortuna !!!</h1>
+          <h3>Animate a saber lo que te prepara el destino</h3>
           <h2>{fortune}</h2>
-          <button onClick={handleClick}>Get Fortune</button>
+          <button onClick={handleClick}
+            className='bg-red-400 text-white font-bold py-2 px-4 rounded'
+          >Consultar</button>
         </article>
       </section>
     </main>
