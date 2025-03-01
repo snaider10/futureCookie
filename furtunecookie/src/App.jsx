094e9d35567ff761b4ebff5cc60ee4bf56997a49
Darkin03 <esnaiderortega@unisinu.edu.co>
2025-03-01 01:34:23 -0500

    First Commit

---
+++ furtunecookie/src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [fortune, setFortune] = useState('')
+  const [active, setActive] = useState(false)
+
+  const handleClick = () => {
+    setActive(true)
+  }
+
+  useEffect(() => {
+    if (active) {
+      try {
+        const getFortune = async () => {
+          const response = await fetch('https://api.adviceslip.com/advice')
+          const data = await response.json()
+          setFortune(data.slip.advice)
+          setActive(false)
+        }
+        getFortune()
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+  },[active])
+  return (
+    <>
+      <h1>This is your fortune!</h1>
+      <h2>{fortune}</h2>
+      <button onClick={handleClick}>Get Fortune</button>
+    </>
+  )
+}
+
+export default App
